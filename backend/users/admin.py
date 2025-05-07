2fad74d124da70c4dad3a828cd2f628adf6f9eb6
Grechkin Vladislav <vladik.grechkin22@gmail.com>
2025-05-07 20:41:34 +0300

    change_comments on admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -3,13 +3,10 @@ from .models import CustomUser
 
 
 class CustomUserAdmin(admin.ModelAdmin):
-    # Указываем, какие поля будут отображаться в списке пользователей
-    list_display = ("username", "email", "first_name", "last_name",
-                    "date_joined")
+    list_display = ("username", "email", "first_name", "last_name", "date_joined")
 
-    # Указываем, по каким полям можно будет производить поиск
+    # Поиск по никнейму и email
     search_fields = ("username", "email")
 
 
-# Регистрируем модель с кастомным администратором
 admin.site.register(CustomUser, CustomUserAdmin)
